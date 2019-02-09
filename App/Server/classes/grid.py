67775c573fe5ea63f1886942486128403f0017b3
Illin Tori <IllinTori@gmail.com>
2019-02-09 12:08:28 +0000

    move to desktop

--- App/Server/classes/grid.py
+++ App/Server/classes/grid.py
@@ -2,17 +2,17 @@ import perlin
 import time
 import random
 import ansi
-x = int(268/2)
+x = 268
 y = 1000
 
 bg = "green"
 
 recources = {
-    "iron":     {"weight":0.15,"char":"  ","colour":"white"},
-    "copper":   {"weight":0.20,"char":"  ","colour":"yellow"},
-    "silicone": {"weight":0.30,"char":"  ","colour":"magenta"},
-    "water":    {"weight":0.60,"char":"  ","colour":"blue"},
-    "oil":      {"weight":0.08,"char":"  ","colour":"black"}
+    "iron":     {"weight":0.15,"zoom":40,"char":" ","colour":"white"},
+    "copper":   {"weight":0.20,"zoom":40,"char":" ","colour":"yellow"},
+    "silicone": {"weight":0.10,"zoom":20,"char":" ","colour":"magenta"},
+    "water":    {"weight":0.40,"zoom":80,"char":" ","colour":"blue"},
+    "oil":      {"weight":0.08,"zoom":40,"char":" ","colour":"black"}
 }
 
 rec_list = [recources["water"],recources["silicone"],recources["copper"],recources["iron"],recources["oil"]]
@@ -20,10 +20,10 @@ rec_list = [recources["water"],recources["silicone"],recources["copper"],recourc
 x_off = 1000000
 y_off = 1000000
 
-hoff = 10900000
+hoff = 0
 
 zoom = 40
-weight = ["  ","░░","▒▒","▓▓","▉▉"]
+weight = ["  ","░░","▒▒","▓▓","▉"]
 
 a = perlin.SimplexNoise()
 ansi.set_text("white")
@@ -34,7 +34,7 @@ for i in range(y):
         flag = False
         for c, r in enumerate(rec_list):
             if not flag:
-                point = a.noise2((j+x_off*c)/zoom, (i+y_off*c)/zoom)
+                point = a.noise2((j*0.5+x_off*c)/r["zoom"], (i+y_off*c)/r["zoom"])
                 if point > 1-r["weight"]:
                     ansi.set_background(r["colour"])
                     print(r["char"],end="")
@@ -42,7 +42,7 @@ for i in range(y):
                     ansi.set_background("black")
         if not flag:
             ansi.set_background(bg)
-            print("  ",end="")
+            print(" ",end="")
             ansi.set_background("black")
         
     print("")
